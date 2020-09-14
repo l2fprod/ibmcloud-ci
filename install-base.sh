d29da4c0419c23de86ff0cc126e3d25632fc6300
Frederic Lavigne <frederic.lavigne@ibm.com>
2020-09-14 09:23:37 -0500

    add tfswitch

--- install-base.sh
+++ install-base.sh
@@ -71,6 +71,9 @@ unzip terraform_0.11.14_linux_amd64.zip terraform
 mv terraform /usr/local/bin/terraform-0.11.14
 rm -f terraform_0.11.14_linux_amd64.zip
 
+echo "TFSwitch"
+curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
+
 # Kubernetes
 echo ">> kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl --retry 10 --retry-delay 5 -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
